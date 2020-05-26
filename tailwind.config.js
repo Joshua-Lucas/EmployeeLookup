a0d0ebc68430bf7d1ce1e89effd1d33b6970fe0f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-26 19:37:18 -0400

    Add brand standards css (color and headings)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        'Primary': '#002d40',
+        'Secondary': '#DFA928',
+        'Tertiary' : '#5855b',
+        'Accent' : '#4e9a92'
+      },
+    },
   },
   variants: {},
   plugins: [],
