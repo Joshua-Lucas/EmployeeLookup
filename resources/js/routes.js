4bd41266fe8f62bc1d9945263dfd17a9860c32e9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-27 21:53:09 -0400

    add 404 component and connect a route

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,14 +1,21 @@
 import Search from './components/Search';
 import SearchResults from './components/SearchResults';
+import NotFound from './components/NotFound';
 
 export default {
         mode: 'history',
 
         routes: [
             {
+                path:'*',
+                component: NotFound
+            },
+            
+            {
                 path:'/',
                 component: Search
             },
+
             {
                 path: '/results',
                 component: SearchResults
