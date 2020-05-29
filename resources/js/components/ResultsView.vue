6f946518e1fd8d9d7e8b81a7153f962647a7b5dd
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-28 20:45:41 -0400

    update routes and creat view to keep history of every search

---
+++ resources/js/components/ResultsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <search-vue></search-vue>
+  </div>
+</template>
+
+<script>
+import SearchVue from "./Search.vue";
+export default {
+  components: {
+    SearchVue
+  }
+};
+</script>
+
